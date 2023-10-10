ccf8a31f2831d710581295f6e7ea8e6f772dab91
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-10-10 18:17:59 +0200

    add ontology __init__

--- ontomap/ontology/__init__.py
+++ ontomap/ontology/__init__.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
-# from ontomap.ontology.anatomy import MouseHumanOMDataset
-# from ontomap.ontology.biodiv import (
-#     EnvoSweetOMDataset, FishZooplanktonOMDataset,
-#     MacroalgaeMacrozoobenthosOMDataset,
-#     TaxrefldBacteriaNcbitaxonBacteriaOMDataset,
-#     TaxrefldChromistaNcbitaxonChromistaOMDataset,
-#     TaxrefldFungiNcbitaxonFungiOMDataset,
-#     TaxrefldPlantaeNcbitaxonPlantaeOMDataset,
-#     TaxrefldProtozoaNcbitaxonProtozoaOMDataset)
-# from ontomap.ontology.food import CiqualSirenOMDataset
-# from ontomap.ontology.phenotype import DoidOrdoOMDataset, HpMpOMDataset
-#
-# ontology_matching = {
-#     # "anatomy": [MouseHumanOMDataset],
-#     # "food": [CiqualSirenOMDataset],
-#     # "biodiv": [
-#     # EnvoSweetOMDataset,
-#     # FishZooplanktonOMDataset,
-#     # MacroalgaeMacrozoobenthosOMDataset,
-#     # TaxrefldBacteriaNcbitaxonBacteriaOMDataset,
-#     # TaxrefldChromistaNcbitaxonChromistaOMDataset,
-#     # TaxrefldFungiNcbitaxonFungiOMDataset,
-#     # TaxrefldPlantaeNcbitaxonPlantaeOMDataset,
-#     # TaxrefldProtozoaNcbitaxonProtozoaOMDataset
-#     # ],
-#     # "phenotype":[
-#     # DoidOrdoOMDataset,
-#     # HpMpOMDataset
-#     # ]
-# }
-#
-# __all__ = ["ontology_matching"]
+from ontomap.ontology.anatomy import MouseHumanOMDataset
+from ontomap.ontology.biodiv import (
+    EnvoSweetOMDataset, FishZooplanktonOMDataset,
+    MacroalgaeMacrozoobenthosOMDataset,
+    TaxrefldBacteriaNcbitaxonBacteriaOMDataset,
+    TaxrefldChromistaNcbitaxonChromistaOMDataset,
+    TaxrefldFungiNcbitaxonFungiOMDataset,
+    TaxrefldPlantaeNcbitaxonPlantaeOMDataset,
+    TaxrefldProtozoaNcbitaxonProtozoaOMDataset)
+from ontomap.ontology.commonkg import (NellDbpediaOMDataset,
+                                       YagoWikidataOMDataset)
+from ontomap.ontology.food import CiqualSirenOMDataset
+from ontomap.ontology.largebio import FMANCIOMDataset
+from ontomap.ontology.phenotype import DoidOrdoOMDataset, HpMpOMDataset
+
+ontology_matching = {
+    "anatomy": [MouseHumanOMDataset],
+    "food": [CiqualSirenOMDataset],
+    "biodiv": [
+        EnvoSweetOMDataset,
+        FishZooplanktonOMDataset,
+        MacroalgaeMacrozoobenthosOMDataset,
+        TaxrefldBacteriaNcbitaxonBacteriaOMDataset,
+        TaxrefldChromistaNcbitaxonChromistaOMDataset,
+        TaxrefldFungiNcbitaxonFungiOMDataset,
+        TaxrefldPlantaeNcbitaxonPlantaeOMDataset,
+        TaxrefldProtozoaNcbitaxonProtozoaOMDataset,
+    ],
+    "phenotype": [DoidOrdoOMDataset, HpMpOMDataset],
+    "commonkg": [NellDbpediaOMDataset, YagoWikidataOMDataset],
+    "largebio": [FMANCIOMDataset],
+}
+
+__all__ = ["ontology_matching"]
