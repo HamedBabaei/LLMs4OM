49849930365c3e149bbb7ddd8e3137e20afd8c6e
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-10-10 12:07:22 +0200

    add ontologies

--- ontomap/__init__.py
+++ ontomap/__init__.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-#
-# __version__ = "0.1.0"
-#
-# import logging
-#
-# from ontomap import base, ontology, utils
-#
-# __all__ = ["base", "ontology", "utils"]
-#
-# # Root logger configuration
-# logger = logging.getLogger(__name__)
-# logger.setLevel(logging.DEBUG)
-#
-# stdout = logging.StreamHandler()
-# stdout.setLevel(logging.DEBUG)
-#
-# formatter = logging.Formatter("%(name)s - %(levelname)s: %(message)s")
-# stdout.setFormatter(formatter)
-#
-# logger.addHandler(stdout)
+
+__version__ = "0.1.0"
+
+import logging
+
+from ontomap import base, ontology, utils
+
+__all__ = ["base", "ontology", "utils"]
+
+# Root logger configuration
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+stdout = logging.StreamHandler()
+stdout.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(name)s - %(levelname)s: %(message)s")
+stdout.setFormatter(formatter)
+
+logger.addHandler(stdout)
