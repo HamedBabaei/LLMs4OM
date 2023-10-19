eab16b097e25985cd5bc77e0bac9b64499497e17
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-10-19 15:51:34 +0200

    add flant5 configs

--- ontomap/base/configs.py
+++ ontomap/base/configs.py
@@ -4,12 +4,14 @@
 """
 import argparse
 import os
+from typing import Optional
 
 
 class BaseConfig:
-    def __init__(self, root_dataset_dir):
+    def __init__(self, root_dataset_dir: str, approach: Optional[str] = "none"):
         self.root_dataset_dir = root_dataset_dir
         self.parser = argparse.ArgumentParser()
+        self.approach = approach
 
     def mkdir(self, path):
         if not os.path.exists(path):
@@ -20,6 +22,13 @@ class BaseConfig:
 
     def __str__(self):
         return "config."
+
+    def flant5(self, device: str):
+        if self.approach == "baseline":
+            config = {"max_token_length": 500, "num_beams": 10, "device": device}
+        else:
+            config = {}
+        return config
 
     def get_args(self, device="cpu"):
         self.parser.add_argument(
@@ -35,7 +44,7 @@ class BaseConfig:
         self.parser.add_argument(
             "--FlanT5",
             type=dict,
-            default={"max_token_length": 500, "num_beams": 10, "device": device},
+            default=self.flant5(device),
         )
         self.parser.add_argument("-f")
         return self.parser.parse_args()
