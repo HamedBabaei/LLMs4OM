b397dfdefba0ecd34e36e8e6712ca5ab5dc46948
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-10-30 16:13:00 +0100

    fix script

--- scripts/run_outofbox_baselines.sh
+++ scripts/run_outofbox_baselines.sh
@@ -2,20 +2,12 @@
 
 approach="out-of-box"
 use_all_approach_prompts=False
-approach_prompts_to_consider=('iri-label' 'iri-label-description' 'iri-label-children' 'iri-label-parent')
+approach_prompts_to_consider="['iri-label', 'iri-label-description', 'iri-label-children', 'iri-label-parent']"
 use_all_llm=False
-llms_to_consider=("FlanT5" "LLaMA7B" "LLaMA13B" "Wizard13B" "Mistral7B" "ChatGPT" "GPT4")
+llms_to_consider="['FlanT5','LLaMA7B','LLaMA13B','Wizard13B','Mistral7B', 'ChatGPT', 'GPT4']"
 load_from_json=True
-device="cpu"
+device="cuda:1"
 
-# A function to join array elements with commas
-join_by_comma() {
-    local IFS=","
-    echo "$*"
-}
-
-approach_prompts_to_consider_string="$(join_by_comma "${approach_prompts_to_consider[@]}")"
-llms_to_consider_string="$(join_by_comma "${llms_to_consider[@]}")"
 
 cd ..
 
@@ -24,9 +16,9 @@ from ontomap import OntoMapPipeline
 args = {
     'approach': '$approach',
     'use-all-approach-prompts': $use_all_approach_prompts,
-    'approach-prompts-to-consider': ['$approach_prompts_to_consider_string'],
+    'approach-prompts-to-consider': $approach_prompts_to_consider,
     'use-all-llm': $use_all_llm,
-    'llms-to-consider': ['$llms_to_consider_string'],
+    'llms-to-consider': $llms_to_consider,
     'load-from-json': $load_from_json,
     'device': '$device',
 }
