854d6efc017c38e68a858c51cb3549f58325f2c0
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-11-18 00:01:06 +0100

    improve config structures

--- ontomap/base/configs.py
+++ ontomap/base/configs.py
@@ -30,32 +30,32 @@ class BaseConfig:
         return "config."
 
     def flan_t5(self, device: str) -> Dict:
+        config = {
+            "max_token_length": 5000,
+            "truncation": False,
+            "num_beams": 1,
+            "device": device,
+            "top_p": 0.95,
+            "temperature": 0.8,
+        }
         if self.approach == "naiv-conv-oaei":
-            config = {
-                "max_token_length": 5000,
-                "truncation": False,
-                "tokenizer_max_length": 4096,
-                "num_beams": 1,
-                "device": device,
-                "top_p": 0.95,
-                "temperature": 0.8,
-            }
+            config["tokenizer_max_length"] = 4096
         else:
             config = {}
         return config
 
     def llama(self, device: str) -> Dict:
+        config = {
+            "max_token_length": 5000,
+            "num_beams": 1,
+            "device": device,
+            "truncation": False,
+            "HUGGINGFACE_ACCESS_TOKEN": os.environ["HUGGINGFACE_ACCESS_TOKEN"],
+            "top_p": 0.95,
+            "temperature": 0.8,
+        }
         if self.approach == "naiv-conv-oaei":
-            config = {
-                "max_token_length": 5000,
-                "num_beams": 1,
-                "tokenizer_max_length": 4096,
-                "device": device,
-                "truncation": False,
-                "HUGGINGFACE_ACCESS_TOKEN": os.environ["HUGGINGFACE_ACCESS_TOKEN"],
-                "top_p": 0.95,
-                "temperature": 0.8,
-            }
+            config["tokenizer_max_length"] = 4096
         else:
             config = {}
         return config
@@ -64,18 +64,18 @@ class BaseConfig:
         openai.api_key = os.environ["OPENAI_KEY"]
         # due to the privacy I had to set key here,
         # but the correct way is to set inside OPENAILLMARCH class
+        config = {
+            "sleep": 10,
+            "top_p": 0.95,
+            "temperature": 0.8,
+        }
         if self.approach == "naiv-conv-oaei":
-            config = {
-                "sleep": 10,
-                "max_token_length": 5000,
-                "top_p": 0.95,
-                "temperature": 0.8,
-            }
+            config["max_token_length"] = 5000
         else:
             config = {}
         return config
 
-    def lightweight_config(self) -> Dict:
+    def fuzzy(self) -> Dict:
         config = {"fuzzy_sm_threshold": 0.8}
         return config
 
@@ -95,6 +95,8 @@ class BaseConfig:
             type=str,
             default=os.path.join(self.root_dataset_dir, "experiments", "stats"),
         )
+
+        # LLM configuration
         flan_t5_config = self.flan_t5(device)
         llama_config = self.llama(device)
         gpt_config = self.gpt()
@@ -106,14 +108,15 @@ class BaseConfig:
         self.parser.add_argument("--ChatGPT", type=dict, default=gpt_config)
         self.parser.add_argument("--GPT4", type=dict, default=gpt_config)
 
-        self.parser.add_argument(
-            "--SimpleFuzzySM", type=dict, default=self.lightweight_config()
-        )
-        self.parser.add_argument(
-            "--WeightedFuzzySM", type=dict, default=self.lightweight_config()
-        )
-        self.parser.add_argument(
-            "--TokenSetFuzzySM", type=dict, default=self.lightweight_config()
-        )
+        # Lightweight Configuration
+        fuzzy_config = self.fuzzy()
+        self.parser.add_argument("--SimpleFuzzySM", type=dict, default=fuzzy_config)
+        self.parser.add_argument("--WeightedFuzzySM", type=dict, default=fuzzy_config)
+        self.parser.add_argument("--TokenSetFuzzySM", type=dict, default=fuzzy_config)
+
+        # Retrieval Configurations
+
+        # RAG configurations
+
         self.parser.add_argument("-f")
         return self.parser.parse_args()
