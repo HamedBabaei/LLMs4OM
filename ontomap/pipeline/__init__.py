80c155f6441606810c13f616c9b8d82b521b7e7e
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-11-19 02:07:37 +0100

    add retriever

--- ontomap/pipeline/__init__.py
+++ ontomap/pipeline/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from ontomap.pipeline.oaei_pipeline import OAEIOMPipeline
 
-OMPipelines = {"naiv-conv-oaei": OAEIOMPipeline, "lightweight": OAEIOMPipeline}
+OMPipelines = {
+    "naiv-conv-oaei": OAEIOMPipeline,
+    "lightweight": OAEIOMPipeline,
+    "retrieval": OAEIOMPipeline,
+}
 
 __all__ = ["OMPipelines"]
