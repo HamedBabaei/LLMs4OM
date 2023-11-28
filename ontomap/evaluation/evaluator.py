b7943e9b44314c77933b36bbba809bba2df90947
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2023-11-28 23:03:16 +0100

    mv ir reformer to uitls

--- ontomap/evaluation/evaluator.py
+++ ontomap/evaluation/evaluator.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from typing import Any, Dict, List
 
-from tqdm import tqdm
-
 from ontomap.evaluation.metrics import evaluation_report
+from ontomap.utils.output_reformer import refactor_retrieval_predicts
 
 
 def evaluator(track: str, predicts: List, references: Any):
@@ -54,32 +53,6 @@ def evaluator(track: str, predicts: List, references: Any):
     else:
         results = evaluation_report(predicts=predicts, references=references)
     return results
-
-
-def refactor_retrieval_predicts(predicts: List) -> List:
-    predicts_temp = []
-    predict_map = {}
-    for predict in tqdm(predicts):
-        source = predict["source"]
-        target_cands = predict["target-cands"]
-        score_cands = predict["score-cands"]
-        for target, score in zip(target_cands, score_cands):
-            if score > 0:
-                adjusted = False
-                # for predict_temp in predicts_temp:
-                #     if (
-                #         predict_temp["source"] == source
-                #         and predict_temp["target"] == target
-                #     ):
-                if predict_map.get(f"{source}-{target}", "NA") != "NA":
-                    adjusted = True
-                    break
-                if not adjusted:
-                    predicts_temp.append(
-                        {"source": source, "target": target, "score": score}
-                    )
-                    predict_map[f"{source}-{target}"] = f"{source}-{target}"
-    return predicts_temp
 
 
 def evaluator_module(
