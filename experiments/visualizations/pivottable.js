c3f6eaa933b14c09f3efe4ff2f6e039a82be7ac8
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-01-08 11:37:56 +0100

    add save figure option to the pivottable

--- experiments/visualizations/pivottable.js
+++ experiments/visualizations/pivottable.js
@@ -8,7 +8,7 @@ function fetchSelectedCSVAndRenderPivotTable(){
         .then(response => response.text())
         .then(csvData => {
             // Generate Pivot Table
-            $("#output").pivotUI(
+            $("#pivotTableContainer").pivotUI(
                 $.csv.toArrays(csvData),
                 {
                     renderers: $.extend(
@@ -26,6 +26,30 @@ function fetchSelectedCSVAndRenderPivotTable(){
         });
 }
 
+
+function capturePivotTable() {
+    // Use html2canvas to capture the content inside the pivotTableContainer
+    html2canvas(document.getElementById('pivotTableContainer')).then(function(canvas) {
+        // Ask the user to specify the filename
+        var fileName = window.prompt('Enter file name', 'pivot_table_capture.png');
+
+        // If the user provides a filename and doesn't cancel the prompt
+        if (fileName) {
+            // Convert canvas to an image data URL
+            var image = canvas.toDataURL(); // This is the image data
+
+            // Create a link (<a>) element
+            var downloadLink = document.createElement('a');
+            downloadLink.href = image;
+            downloadLink.download = fileName; // Use the specified filename
+
+            // Simulate a click on the download link
+            downloadLink.click();
+        }
+    });
+}
+
 document.getElementById('csvFiles').addEventListener('change', fetchSelectedCSVAndRenderPivotTable);
+document.getElementById('captureButton').addEventListener('click', capturePivotTable);
 
 fetchSelectedCSVAndRenderPivotTable();
