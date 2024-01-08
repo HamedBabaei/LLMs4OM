8883f02268e9e520b16e530684b2b0cb0f1d6357
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-01-08 11:38:27 +0100

    add a hybrid postprocessor

--- ontomap/postprocess/process.py
+++ ontomap/postprocess/process.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from typing import Dict, List
 
+import numpy as np
 from tqdm import tqdm
 
 
@@ -117,7 +118,7 @@ def confidence_score_based_filtering(
     return filtered_predicts
 
 
-def postprocess(
+def postprocess_heuristic(
     predicts: List, topk_confidence_ratio: int = 3, topk_confidence_score: int = 1
 ) -> [List, Dict]:
     ir_outputs = predicts[0]["ir-outputs"]
@@ -156,3 +157,125 @@ def postprocess(
         "llm-confidence-th": llm_confidence_threshold,
     }
     return filtered_predicts, configs
+
+
+def postprocess_hybrid(
+    predicts: List, ir_score_threshold: float = 0.9, llm_confidence_th: float = 0.7
+) -> [List, Dict]:
+    ir_outputs = predicts[0]["ir-outputs"]
+    llm_outputs = predicts[1]["llm-output"]
+    ir_cleaned_outputs_id = []
+    ir_cleaned_outputs = []
+    for ir in ir_outputs:
+        if ir["source"] not in ir_cleaned_outputs_id:
+            ir_cleaned_outputs_id.append(ir["source"])
+            ir_cleaned_outputs.append(ir)
+    ir_outputs = ir_cleaned_outputs
+    # ir_outputs = preprocess_ir_outputs(predicts=ir_outputs)
+    targets = [
+        target
+        for index, ir_output in enumerate(ir_outputs)
+        for target in ir_output["target-cands"]
+    ]
+    targets = list(set(targets))
+    target2index = {target: index for index, target in enumerate(targets)}
+    source2index = {
+        ir_output["source"]: index for index, ir_output in enumerate(ir_outputs)
+    }
+
+    ir_dict = {ir_output["source"]: ir_output for ir_output in ir_outputs}
+    ir_dict_based_llm = {
+        ir_output["source"]: np.zeros(len(target2index)) for ir_output in ir_outputs
+    }
+    llm_dict = {
+        ir_output["source"]: np.zeros(len(target2index)) for ir_output in ir_outputs
+    }
+
+    outputdict = {}
+
+    # llm_confidence_th = 0.7
+    for llm_output in tqdm(llm_outputs):
+        if llm_output["source"] not in list(outputdict.keys()):
+            outputdict[llm_output["source"]] = []
+        if llm_output["score"] > llm_confidence_th:
+            ir_output = ir_dict.get(llm_output["source"])
+            for index, (ir_cand, ir_cand_score) in enumerate(
+                zip(ir_output["target-cands"], ir_output["score-cands"])
+            ):
+                if ir_cand == llm_output["target"]:
+                    confidence_ratio = (
+                        llm_output["score"] * 0.2 + 0.8 * ir_cand_score
+                    ) / 2
+                    predicts_list = [
+                        llm_output["target"],
+                        ir_cand_score,
+                        llm_output["score"],
+                        confidence_ratio,
+                    ]
+                    outputdict[llm_output["source"]].append(predicts_list)
+                    ir_dict_based_llm[llm_output["source"]][
+                        target2index[ir_cand]
+                    ] = ir_cand_score
+                    llm_dict[llm_output["source"]][
+                        target2index[ir_cand]
+                    ] = confidence_ratio
+                    # llm_output['score']
+                    break
+
+    ir_matrix = np.zeros((len(source2index), len(target2index)))
+
+    for iri, output in ir_dict.items():
+        for ir_cand, ir_score in zip(output["target-cands"], output["score-cands"]):
+            ir_matrix[source2index[iri], target2index[ir_cand]] = ir_score
+
+    ir_matrix_based_llm = np.zeros((len(source2index), len(target2index)))
+    for iri, output in ir_dict_based_llm.items():
+        ir_matrix_based_llm[source2index[iri], :] = output
+
+    llm_matrix = np.zeros((len(source2index), len(target2index)))
+    for iri, output in llm_dict.items():
+        llm_matrix[source2index[iri], :] = output
+
+    for col_idx in range(ir_matrix_based_llm.shape[1]):
+        col = ir_matrix_based_llm[:, col_idx]
+        max_index = np.argmax(col)
+        ir_matrix_based_llm[:, col_idx] = np.where(
+            np.arange(len(col)) != max_index, 0, col
+        )
+
+    for col_idx in range(llm_matrix.shape[1]):
+        col = llm_matrix[:, col_idx]
+        max_index = np.argmax(col)
+        llm_matrix[:, col_idx] = np.where(np.arange(len(col)) != max_index, 0, col)
+
+    for row_idx in range(ir_matrix_based_llm.shape[0]):
+        row = ir_matrix_based_llm[row_idx, :]
+        max_index = np.argmax(row)
+        ir_matrix_based_llm[row_idx, :] = np.where(
+            np.arange(len(row)) != max_index, 0, row
+        )
+
+    for row_idx in range(llm_matrix.shape[0]):
+        row = llm_matrix[row_idx, :]
+        max_index = np.argmax(row)
+        llm_matrix[row_idx, :] = np.where(np.arange(len(row)) != max_index, 0, row)
+
+    index2source = {index: source for source, index in source2index.items()}
+    index2target = {index: target for target, index in target2index.items()}
+    rows, cols = ir_matrix_based_llm.nonzero()
+    final_predict = []
+    # ir_score_threshold = 0.9
+    for row, col in zip(rows, cols):
+        if ir_matrix_based_llm[row, col] >= ir_score_threshold:
+            final_predict.append(
+                {
+                    "source": index2source[row],
+                    "target": index2target[col],
+                    "score": ir_matrix_based_llm[row, col],
+                }
+            )
+    configs = {
+        "llm-confidence-th": llm_confidence_th,
+        "ir-score-th": ir_score_threshold,
+    }
+    return final_predict, configs
