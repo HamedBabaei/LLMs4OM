8fbb1012ab63770532a9ed9a38db294046119840
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-01-18 17:39:10 +0100

    remove comments

--- ontomap/pipeline/__init__.py
+++ ontomap/pipeline/__init__.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
 from ontomap.pipeline.om_pipeline import OMPipelines
 
-# OMPipelines = {
-#     "naiv-conv-oaei": OAEIOMPipeline,
-#     "lightweight": OAEIOMPipeline,
-#     "retrieval": OAEIOMPipeline,
-#     "rag": OAEIOMPipeline,
-#     "icv": OAEIOMPipeline,
-# }
-
 __all__ = ["OMPipelines"]
